66182365c560fa8066b28d99774106e1100361f0
raidelo <raidelosix@gmail.com>
2024-08-01 19:22:18 -0400

    Mejoras

--- dns_proxy.py
+++ dns_proxy.py
@@ -1,4 +1,5 @@
-from dnslib.server import DNSServer, DNSHandler, DNSLogger, RR
+from time import strftime
+from dnslib.server import DNSServer, DNSHandler, DNSLogger, RR, RCODE, QTYPE
 from dnslib.proxy import ProxyResolver
 from configparser import ConfigParser
 
@@ -38,9 +39,47 @@ class MainResolver(ProxyResolver):
     def get_domain(self, qname):
         return b'.'.join(qname.label).decode('utf-8')
 
+class MainLogger(DNSLogger):
+    def log_prefix(self, handler):
+        if self.prefix:
+            return "%s " % (strftime("%Y-%m-%d %X"))
+        else:
+            return ""
+
+    def log_request(self, handler, request):
+        self.logf("%sRequest: [%s:%d] (%s) / '%s' (%s)" % (
+                    self.log_prefix(handler),
+                    handler.client_address[0],
+                    handler.client_address[1],
+                    handler.protocol,
+                    request.q.qname,
+                    QTYPE[request.q.qtype]))
+        self.log_data(request)
+    
+    def log_reply(self, handler, reply):
+        if reply.header.rcode == RCODE.NOERROR:
+            self.logf("%sReply:   [%s:%d] (%s) / '%s' (%s) / RRs: %s" % (
+                    self.log_prefix(handler),
+                    handler.client_address[0],
+                    handler.client_address[1],
+                    handler.protocol,
+                    reply.q.qname,
+                    QTYPE[reply.q.qtype],
+                    ",".join([QTYPE[a.rtype] for a in reply.rr])))
+        else:
+            self.logf("%sReply:   [%s:%d] (%s) / '%s' (%s) / %s" % (
+                    self.log_prefix(handler),
+                    handler.client_address[0],
+                    handler.client_address[1],
+                    handler.protocol,
+                    reply.q.qname,
+                    QTYPE[reply.q.qtype],
+                    RCODE[reply.header.rcode]))
+        self.log_data(reply)
+
 def main(local_server_address=('0.0.0.0', 53), dns_server_address=('1.1.1.1', 53), timeout=5, log='', log_prefix=False, map={}, exceptions={}):
     resolver = MainResolver(*dns_server_address, timeout=timeout, map=map, exceptions=exceptions)
-    logger = DNSLogger(log, log_prefix)
+    logger = MainLogger(log, log_prefix)
     server_ = DNSServer(resolver, *local_server_address, logger=logger, handler=DNSHandler)
     try:
         server_.start()
@@ -50,23 +89,30 @@ def main(local_server_address=('0.0.0.0', 53), dns_server_address=('1.1.1.1', 53
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
+    defaults = {'address':'0.0.0.0',
+                'port':53,
+                'upstream':'1.1.1.1:53',
+                'timeout':5,
+                'log':'request,reply,truncated,error',
+                'log_prefix':False,
+                }
     parser = ArgumentParser()
-    parser.add_argument('-b', '-a', '--bind', '--address', default='0.0.0.0', metavar='ADDRESS', dest='address',
+    parser.add_argument('-b', '-a', '--bind', '--address', default=defaults['address'], metavar='ADDRESS', dest='address',
                         help='bind to this address '
                         '(default: all interfaces)')
-    parser.add_argument('port', default=53, type=int, nargs='?',
+    parser.add_argument('port', default=defaults['port'], type=int, nargs='?',
                         help='bind to this port '
                         '(default: %(default)s)')
-    parser.add_argument('-u', '--upstream', default='1.1.1.1:53', metavar='<dns server:port>', dest='upstream',
+    parser.add_argument('-u', '--upstream', default=defaults['upstream'], metavar='<dns server:port>', dest='upstream',
                         help='upstream DNS server:port '
                         '(default: %(default)s)')
-    parser.add_argument('-t', '--timeout', default=5, type=int, dest='timeout',
+    parser.add_argument('-t', '--timeout', default=defaults['timeout'], type=int, dest='timeout',
                         help='timeout for the server to resolve queries '
                         '(default: %(default)s)')
-    parser.add_argument('--log', default='request,reply,truncated,error', dest='log',
+    parser.add_argument('--log', default=defaults['log'], dest='log',
                         help='log hooks to enable '
                         '(default: +request,+reply,+truncated,+error,-recv,-send,-data)')
-    parser.add_argument('--log-prefix', action='store_true', default=False, dest='log_prefix',
+    parser.add_argument('--log-prefix', action='store_true', default=defaults['log_prefix'], dest='log_prefix',
                         help='log prefix (timestamp/handler/resolver) '
                         '(default: %(default)s)')
     parser.add_argument('--save-config', nargs='?', default=False, const='dns_proxy_settings.ini', type=str, dest='save_config',
@@ -83,50 +129,43 @@ if __name__ == '__main__':
                         '(default: %(default)s)')
     args = parser.parse_args()
 
-    defaults = {'address':'0.0.0.0',
-                'port':53,
-                'upstream':'1.1.1.1:53',
-                'timeout':5,
-                'log':'request,reply,truncated,error',
-                'log_prefix':False,
-                }
     map_ = get_mapping(args.map)
     exceptions_ = get_mapping(args.exceptions)
 
-    c_parser = ConfigParser(defaults=defaults)
-    c_parser.add_section('SAVED')
-    c_parser.add_section('MAP')
-    c_parser.add_section('EXCEPTIONS')
-    config_file_route = args.save_config if args.save_config else 'dns_proxy_settings.ini'
-    if args.save_config:
-        for arg, xdefect_value in defaults.items():
-            dict_args = dict(args._get_kwargs())
-            if dict_args[arg] != xdefect_value:
-                c_parser.set('SAVED', arg, str(dict_args[arg]))
-        if map_:
-            c_parser['MAP'] = map_
-        if exceptions_:
-            c_parser['EXCEPTIONS'] = exceptions_
-        with open(config_file_route, 'w') as file:
-            c_parser.write(file)
+    if args.save_config or not args.use_args:
+        c_parser = ConfigParser(defaults=defaults)
+        for section in ('SAVED', 'MAP', 'EXCEPTIONS'):
+            c_parser.add_section(section)
+        config_file_route = args.save_config if args.save_config else 'dns_proxy_settings.ini'
+        if args.save_config:
+            for arg, xdefect_value in defaults.items():
+                dict_args = dict(args._get_kwargs())
+                if dict_args[arg] != xdefect_value:
+                    c_parser.set('SAVED', arg, str(dict_args[arg]))
+            if map_:
+                c_parser['MAP'] = map_
+            if exceptions_:
+                c_parser['EXCEPTIONS'] = exceptions_
+            with open(config_file_route, 'w') as file:
+                c_parser.write(file)
 
-    if not args.use_args:
-        found_ = c_parser.read(config_file_route)
-        if found_:
-            address = c_parser.get('SAVED', 'address')
-            port = c_parser.getint('SAVED', 'port')
-            upstream = c_parser.get('SAVED', 'upstream')
-            timeout = c_parser.getint('SAVED', 'timeout')
-            log = c_parser.get('SAVED', 'log')
-            log_prefix = c_parser.getboolean('SAVED', 'log_prefix')
-            map_ = get_section_without_defaults(c_parser, 'MAP')
-            exceptions_ = get_section_without_defaults(c_parser, 'EXCEPTIONS')
-        else:
-            args.use_args = True
+        if not args.use_args:
+            found_ = c_parser.read(config_file_route)
+            if found_:
+                address = c_parser.get('SAVED', 'address')
+                port = c_parser.getint('SAVED', 'port')
+                upstream = c_parser.get('SAVED', 'upstream')
+                timeout = c_parser.getint('SAVED', 'timeout')
+                log = c_parser.get('SAVED', 'log')
+                log_prefix = c_parser.getboolean('SAVED', 'log_prefix')
+                map_ = get_section_without_defaults(c_parser, 'MAP')
+                exceptions_ = get_section_without_defaults(c_parser, 'EXCEPTIONS')
+            else:
+                args.use_args = True
     if args.use_args:
         address, port, upstream, timeout, log, log_prefix = args.address, args.port, args.upstream, args.timeout, args.log, args.log_prefix
 
     upstream_address, _, upstream_port = upstream.partition(':')
     upstream_port = int(upstream_port or 53)
-    print('Server started at %s:%d'%(address, port))
+    print('Server started at %s:%d || Remote server at %s:%d'%(address, port, upstream_address, upstream_port))
     main((address, port), (upstream_address, upstream_port), timeout, log, log_prefix, map_, exceptions_)
